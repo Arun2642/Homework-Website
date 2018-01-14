688827b24bc92ee5ae9ba00f1721cc9f67ec30bf
Arun Johnson <arun2642@gmail.com>
2018-01-13 20:26:37 -0800

    See previous message (I had modified the wrong file)

--- example-app.js
+++ example-app.js
@@ -572,7 +572,7 @@ function printLoginInfo(res, loginInfo) {
     res.end(html);
 }
 
-var port = 8000;
+var port = process.env.PORT || 8000;
 app.listen(port);
 console.log('Server running at http://aruninc.com:%d/', port);
 
